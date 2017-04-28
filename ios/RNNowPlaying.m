2f8df11d64393922d03c3f97f42c7a87dea5c1c4
Wesley Anest <wesley.anest@free.fr>
2017-04-28 15:00:30 +0200

    Fix ios bug

--- ios/RNNowPlaying.m
+++ ios/RNNowPlaying.m
@@ -1,36 +1,30 @@
-
 #import "RNNowPlaying.h"
 #import "RCTEventDispatcher.h"
 
 @implementation RNNowPlaying
 
-RCT_EXPORT_MODULE();
-
 @synthesize bridge = _bridge;
 
+RCT_EXPORT_MODULE();
+- (dispatch_queue_t-methodQueue{
+	return dispatch_get_main_queue();
+}
 
-- (id)init {
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
+- (RNNowPlaying *)init {
+    self = [super init];
+    self.musicPlayer = [MPMusicPlayerController systemMusicPlayer];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(nowPlayingEventReceived:) name:MPMusicPlayerControllerNowPlayingItemDidChangeNotification object:self.musicPlayer];
+     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(nowPlayingEventReceived:) name:MPMusicPlayerControllerPlaybackStateDidChangeNotification object:self.musicPlayer];
         
-        self.musicPlayer = [MPMusicPlayerController systemMusicPlayer];
-        
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(nowPlayingEventReceived:) name:MPMusicPlayerControllerNowPlayingItemDidChangeNotification object:self.musicPlayer];
-        
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(nowPlayingEventReceived:) name:MPMusicPlayerControllerPlaybackStateDidChangeNotification object:self.musicPlayer];
-        
-        [self.musicPlayer beginGeneratingPlaybackNotifications];
-    });
-    
+    [self.musicPlayer beginGeneratingPlaybackNotifications];
+
     return self;
 }
 
 - (void)nowPlayingEventReceived:(NSNotification *)notification
 {
     MPMusicPlayerController *player = (MPMusicPlayerController *)notification.object;
-    // On recupere la musique jouée actuellement
     MPMediaItem *item = [player nowPlayingItem];
-    
     if (self.musicPlayer.playbackState == MPMusicPlaybackStatePlaying){
         [self.bridge.eventDispatcher sendAppEventWithName:@"nowPlayingEvent"
                                                      body:@{@"title": item.title}];
