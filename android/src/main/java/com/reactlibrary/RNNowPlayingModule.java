283ae73e0d44ed6340370bb0f49a81fb668f6ff5
Wesley Anest <wesley.anest@free.fr>
2017-04-28 17:11:16 +0200

    Add missing imports for android

--- android/src/main/java/com/reactlibrary/RNNowPlayingModule.java
+++ android/src/main/java/com/reactlibrary/RNNowPlayingModule.java
@@ -1,9 +1,19 @@
 package com.reactlibrary;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class RNNowPlayingModule extends ReactContextBaseJavaModule {
   public static final int REQUEST_CODE_READ_STORAGE = 180;
