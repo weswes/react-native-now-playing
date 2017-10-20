61cc6e1eb11b242139d4e7fbc951f04d475fe556
Wesley Anest <wesley.anest@free.fr>
2017-10-20 16:21:58 +0200

    remove example

--- Example/index.android.js
+++
@@ -1,7 +0,0 @@
-
-import React, { Component } from 'react';
-import  { AppRegistry } from 'react-native';
-
-import Example from './main';
-
-AppRegistry.registerComponent('Example', () => Example);
