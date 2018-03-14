978d6ecb9e9c8fdeb56c0d7ec2be8434d3485df2
Wesley Anest <wesley.anest@free.fr>
2018-03-14 13:20:53 +0100

    [ios] Remove init and Implement startObserving

--- ios/RNNowPlaying.m
+++ ios/RNNowPlaying.m
@@ -1,5 +1,5 @@
 #import "RNNowPlaying.h"
-#import "RCTEventDispatcher.h"
+
 
 @implementation RNNowPlaying
 
@@ -11,14 +11,12 @@ RCT_EXPORT_MODULE();
     return @[@"NowPlayingEvent"];
 }
 
-- (RNNowPlaying *)init {
-    self.musicPlayer = [MPMusicPlayerController systemMusicPlayer];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(nowPlayingEventReceived:) name:MPMusicPlayerControllerNowPlayingItemDidChangeNotification object:self.musicPlayer];
-     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(nowPlayingEventReceived:) name:MPMusicPlayerControllerPlaybackStateDidChangeNotification object:self.musicPlayer];
-        
-    [self.musicPlayer beginGeneratingPlaybackNotifications];
-
-    return self;
+-(void)startObserving{
+  hasListeners = YES;
+  self.musicPlayer = [MPMusicPlayerController systemMusicPlayer];
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(nowPlayingEventReceived:) name:MPMusicPlayerControllerNowPlayingItemDidChangeNotification object:self.musicPlayer];
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(nowPlayingEventReceived:) name:MPMusicPlayerControllerPlaybackStateDidChangeNotification object:self.musicPlayer];
+  [self.musicPlayer beginGeneratingPlaybackNotifications];
 }
 
 - (void)nowPlayingEventReceived:(NSNotification *)notification
@@ -26,14 +24,14 @@ RCT_EXPORT_MODULE();
     //e.playbackTime, e.playbackDuration, e.title, e.albumTitle, e.artist
     MPMusicPlayerController *player = (MPMusicPlayerController *)notification.object;
     MPMediaItem *item = [player nowPlayingItem];
-    
-    
+
+
     NSString *playbackTime = [NSString stringWithFormat:@"%@", [NSNumber numberWithDouble:player.currentPlaybackTime]];
     NSString *playbackDuration = [NSString stringWithFormat:@"%@", [NSNumber numberWithDouble:item.playbackDuration]];
     NSString *artist = [NSString stringWithFormat:@"%@", item.artist];
     NSString *albumTitle = [NSString stringWithFormat:@"%@", item.albumTitle];
     NSString *title = [NSString stringWithFormat:@"%@", item.title];
-    
+
     if (player.playbackState == MPMusicPlaybackStatePlaying){
         [self sendEventWithName:@"NowPlayingEvent"
                            body:@{@"playbackTime": playbackTime,
@@ -41,10 +39,10 @@ RCT_EXPORT_MODULE();
                                   @"title": title,
                                   @"albumTitle": albumTitle,
                                   @"artist": artist
-                                  
+
                                   }];
     }
-        
+
 
 }
 
