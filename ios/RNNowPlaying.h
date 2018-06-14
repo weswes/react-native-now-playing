144920ef0646d1edfcd77078e3537ed4272cfbe4
Wesley Anest <wesley.anest@free.fr>
2018-06-14 22:26:47 +0200

    ios add spotify framework

--- ios/RNNowPlaying.h
+++ ios/RNNowPlaying.h
@@ -1,9 +1,11 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 #import <MediaPlayer/MediaPlayer.h>
+#import <SpotifyAppRemote/SpotifyAppRemote.h>
 
-@interface RNNowPlaying : RCTEventEmitter <RCTBridgeModule>
+@interface RNNowPlaying : RCTEventEmitter <RCTBridgeModule, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate>
 
 @property (nonatomic, retain) MPMusicPlayerController  *musicPlayer;
+@property (nonatomic, strong) SPTAppRemote *appRemote; // Spotify App Remote
 
 @end
--- ios/RNNowPlaying.m
+++ ios/RNNowPlaying.m
@@ -1,6 +1,5 @@
 #import "RNNowPlaying.h"
 
-
 @implementation RNNowPlaying
 {
   bool hasListeners;
@@ -19,7 +18,11 @@ RCT_EXPORT_MODULE();
     return @[@"NowPlayingEvent"];
 }
 
+/**
+ Register to events
+ **/
 -(void)startObserving{
+    // Check Authorization on MPMediaLibrary
     // Traiter les cas du refus...
     if ([MPMediaLibrary authorizationStatus] == MPMediaLibraryAuthorizationStatusAuthorized){
         [self registerNotif];
@@ -28,6 +31,8 @@ RCT_EXPORT_MODULE();
             [self registerNotif];
         }];
     }
+    
+    [self startObservingSpotify];
 
 }
 
@@ -73,4 +78,86 @@ RCT_EXPORT_MODULE();
 
 }
 
+
+/// SPOTIFY
+-(void)startObservingSpotify{
+    SPTAppRemoteConnectionParamsImageFormat format = SPTAppRemoteConnectionParamsImageFormatAny;
+    SPTAppRemoteConnectionParams *params =
+    [[SPTAppRemoteConnectionParams alloc] initWithClientIdentifier:@"e5dec3e3ce1643909db47c3d396861c8"
+                                                       redirectURI:@"spotifywatchexample://"
+                                                              name:@"Kizbee"
+                                                       accessToken:nil
+                                                  defaultImageSize:CGSizeZero
+                                                       imageFormat:format];
+    
+    self.appRemote = [[SPTAppRemote alloc] initWithConnectionParameters:params
+                                                               logLevel:SPTAppRemoteLogLevelDebug];
+    self.appRemote.delegate = self;
+    
+    [self.appRemote connect];
+    
+    [self startObserving];
+}
+
+
+- (void)appRemote:(SPTAppRemote *)appRemote didFailConnectionAttemptWithError:(NSError *)error
+{
+    // Connection failed
+    NSLog(@"Connection failed %@", error);
+    BOOL spotifyInstalled = [self.appRemote authorizeAndPlayURI:@""];
+    if (!spotifyInstalled) {
+        /*
+         * The Spotify app is not installed.
+         * Use SKStoreProductViewController with [SPTAppRemote spotifyItunesItemIdentifier] to present the user
+         * with a way to install the Spotify app.
+         */
+    }
+}
+
+- (void)appRemote:(SPTAppRemote *)appRemote didDisconnectWithError:(nullable NSError *)error
+{
+    // Connection disconnected
+    NSLog(@"Connection disconnected %@", error);
+}
+
+- (void)playerStateDidChange:(id<SPTAppRemotePlayerState>)playerState
+{
+    if (![playerState.track.name isEqualToString:@"Spotify"] && !playerState.isPaused){
+        NSString *playbackTime = [NSString stringWithFormat:@"%@", [NSNumber numberWithDouble:playerState.playbackPosition]];
+        NSString *playbackDuration = [NSString stringWithFormat:@"%@", [NSNumber numberWithDouble:playerState.track.duration]];
+        NSString *artist = [NSString stringWithFormat:@"%@", playerState.track.artist.name];
+        NSString *albumTitle = [NSString stringWithFormat:@"%@", playerState.track.album.name];
+        NSString *title = [NSString stringWithFormat:@"%@", playerState.track.name];
+        
+        NSLog(@"[Nowplaying] artist: %@", artist);
+        NSLog(@"[Nowplaying] albumTitle: %@", albumTitle);
+        NSLog(@"[Nowplaying] title: %@", title);
+    }
+    
+}
+
+- (void)appRemoteDidEstablishConnection:(SPTAppRemote *)appRemote
+{
+    // Connection was successful, you can begin issuing commands
+    
+    appRemote.playerAPI.delegate = self;
+    [appRemote.playerAPI subscribeToPlayerState:
+     ^(id  _Nullable result, NSError * _Nullable error) {
+         if (error) {
+             NSLog(@"%@", error.localizedDescription);
+         }
+     }];
+}
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+    NSDictionary *params = [self.appRemote authorizationParametersFromURL:url];
+    NSString *token = params[SPTAppRemoteAccessTokenKey];
+    if (token) {
+        self.appRemote.connectionParameters.accessToken = token;
+    } else if (params[SPTAppRemoteErrorDescriptionKey]) {
+        NSLog(@"%@", params[SPTAppRemoteErrorDescriptionKey]);
+    }
+    return YES;
+}
 @end
